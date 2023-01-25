e38f52d874cae8bf00f18bedf66b901e8452a4e0
raulbanuelos <raul.banuelos.diaz@hotmail.com>
2023-01-25 10:42:55 -0600

    tercer commit

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ if (navigator.serviceWorker) {
   if (window.location.href.includes("localhost")) swLocation = "/sw.js"; //Varia según el host
   navigator.serviceWorker.register(swLocation);
 }
+
+function saludar(){
+  alert("Hola mundo");
+}
