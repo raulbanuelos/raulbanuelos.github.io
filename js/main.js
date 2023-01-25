a5b6a0c70425ee0194e80c92a1bf44cc290aa026
Raúl Bañuelos <raul.banuelos.diaz@hotmail.com>
2023-01-25 06:41:56 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Configurar SW
+let swLocation = "sw.js";
+
+if (navigator.serviceWorker) {
+  if (window.location.href.includes("localhost")) swLocation = "/sw.js"; //Varia según el host
+  navigator.serviceWorker.register(swLocation);
+}
